32d6063cd12ac76e01a47905723b1ccfcf5ce6b1
Brian Roper <appdevbri@gmail.com>
2018-08-02 08:11:04 -0400

    added base app state

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget{
+  @override
+  State createState() {
+    return new AppState();
+  }
+}
+
+class AppState extends State<App>{
+  @override
+  Widget build(BuildContext context) {
+    return _buildApp();
+  }
+
+  Widget _buildApp(){
+
+  }
+}
---
+++ lib/views/tap_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class TapScreen extends StatefulWidget{
+
+  @override
+  State createState() {
+    return new TapScreenState();
+  }
+}
+
+class TapScreenState extends State<TapScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return _buildTapScreen();
+  }
+
+  Widget _buildTapScreen(){
+    return new Container(
+      padding: EdgeInsets.all(16.0),
+      child: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new Text("Click to choose your song!", style: new TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),)
+          ],
+        ),
+      ),
+    )
+  }
+}
